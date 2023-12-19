031195c034143ce0b9e45d2bac3a1e54f4e64446
Mariano Heredia <albolocura@gmail.com>
2023-12-18 21:10:27 -0300

    fixup! Copy db and point to volume if env var

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 
-[ -z "$RAILWAY_VOLUME_MOUNT_PATH/db.sqlite3" ] && cp db.sqlite3 $RAILWAY_VOLUME_MOUNT_PATH/db.sqlite3
+[ -f "$RAILWAY_VOLUME_MOUNT_PATH/db.sqlite3" ] && cp db.sqlite3 $RAILWAY_VOLUME_MOUNT_PATH/db.sqlite3
 ls $RAILWAY_VOLUME_MOUNT_PATH
 gunicorn secret_santa.wsgi
